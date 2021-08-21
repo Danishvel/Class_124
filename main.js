432466693d87309ab9881d84e70a76cd18c49e1d
Danishvel <79510005+Danishvel@users.noreply.github.com>
2021-08-21 14:31:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function preload(){
+
+}
+function draw() {
+    
+}
+function setup() {
+    canvas = createCanvas(450, 450);
+    canvas.position(900, 130);
+
+    webcam = createCapture(VIDEO);
+    webcam.size(450, 450);
+
+    posenet = ml5.poseNet(webcam, Modelloaded);
+
+    posenet.on('pose', gotPoses);
+}
+
+function Modelloaded() {
+    console.log("Model Loaded !!!!!!!!!!!!!");
+}
+
+function gotPoses(results) {
+    if (reults.length > 0) {
+        console.log(results);
+    }
+}
